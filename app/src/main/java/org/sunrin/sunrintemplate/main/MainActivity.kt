9cf5b567a66616ed3a0dfe6d0cfee8ee209a4021
hyunhomon <hyunhomon@gmail.com>
2023-10-31 11:28:29 +0900

    feat: recycler view layout setup

--- app/src/main/java/org/sunrin/sunrintemplate/main/MainActivity.kt
+++ app/src/main/java/org/sunrin/sunrintemplate/main/MainActivity.kt
@@ -2,7 +2,12 @@ package org.sunrin.sunrintemplate.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import org.sunrin.sunrintemplate.R
 import org.sunrin.sunrintemplate.databinding.ActivityMainBinding
+import org.sunrin.sunrintemplate.main.account.AccountFragment
+import org.sunrin.sunrintemplate.main.feedback.FeedbackFragment
+import org.sunrin.sunrintemplate.main.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     private var mBinding : ActivityMainBinding ?= null
@@ -13,8 +18,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main, fragment)
+            .commit()
+    }
+
+    fun clickBnv() {
+        binding.bnvMain.setOnItemSelectedListener() {
+            when(it.itemId) {
+                R.id.menu_feedback -> {
+                    replaceFragment(FeedbackFragment())
+                    true
+                }
+                R.id.menu_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.menu_person -> {
+                    replaceFragment(AccountFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
     override fun onStart() {
         super.onStart()
+
+        if(supportFragmentManager.findFragmentById(R.id.fcv_main) == null)
+            replaceFragment(HomeFragment())
+
+        clickBnv()
     }
 
     override fun onDestroy() {
